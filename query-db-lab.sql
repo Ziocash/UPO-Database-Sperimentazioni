bb43e03f2fb10ab7b08b6aa55b66836b40bb3cfe
Ziocash <31541164+Ziocash@users.noreply.github.com>
2021-06-21 17:00:07 +0200

    Update query-db-lab.sql
    [+] Improved documentation
    [·] Updated some queries

--- query-db-lab.sql
+++ query-db-lab.sql
@@ -18,7 +18,8 @@ nazione e essere ordinato lessicograficamente per città.
 5- Viste:
     - nessuna vista richiesta
 6- Controllo del risultato:
-    -
+    - la tabella city contiene un campo country che corrisponde al codice della nazione
+     (city.name = Rome -> city.country = IT -> country.code = IT -> country.name = Italy)
 
 */
 
@@ -34,10 +35,16 @@ Elencare le città attraversate da un fiume (il risultato deve contenere soltant
 2- Proprietà delle informazioni:
     - located.city: character varying(50), accetta valori NULL, chiave esterna
     - located.river: character varying(50), accetta valori NULL, chiave esterna
-3- Sottoproblemi:
+3- Sottoproblemi: 
+    - non risultano sottoproblemi
 4- Assunzioni:
+    - si assume che i dati siano corretti
+    - si assume che essendoci più fiumi nella stessa città la città sia elencata una volta sola
 5- Viste:
+    - nessuna vista richiesta
 6- Controllo del risultato:
+    - la tabella "located" contiene il campo "river", se non è nullo, nella città scorre un fiume
+    (es.: Parigi -> Senna)
 
 */
 
@@ -46,14 +53,25 @@ WHERE river IS NOT NULL
 
 /*
 
-Query 3
+Query 3:
+Elencare le città con un nome che comprende la lettera y in qualsiasi posizione (il risultato deve 
+contenere il nome della città e il nome del paese e essere ordinato per paese e città).
 
-1- Informazioni correlate: 
+1- Informazioni correlate: Nome città, nome nazione, codice nazione e nazione della città
 2- Proprietà delle informazioni: 
+    - city.name: character varying(50), non accetta valori NULL, parte della chiave primaria composta citykey
+    - city.country: character varying(4), non accetta valori NULL; parte della chiave primaria composta citykey, chiave esterna
+    - country.name: character varying(50), non accetta valori NULL, valore univoco, non duplicabile
+    - country.code: character varying(4), non accetta valori NULL, chiave primaria countrykey
 3- Sottoproblemi:
+    - Selezionare il nome della città in base alla lettera y
+    - Selezionare il nome del Paese
 4- Assunzioni:
+    - si assume che i dati siano corretti
 5- Viste:
+    - nessuna vista richiesta
 6- Controllo del risultato:
+    - controllo che tutti i nomi delle città contengano la lettera y in una posizione qualsiasi
 
 */
 
@@ -62,17 +80,25 @@ WHERE city.name LIKE '%y%'
 ORDER BY country.name, city.name
 
 /*
-Query 4
+Query 4:
+Elencare le città che si trovano nei circoli polari artico e antartico, cioè oltre 66 gradi di latitudine 
+nord e sud (il risultato deve contenere le città e la loro latitudine).
 
-1- Informazioni correlate: Nome città e fiume che scorre nella città
+1- Informazioni correlate: Nome città e latitudine della città
 2- Proprietà delle informazioni:
-    - located.city: character varying(50), accetta valori NULL, chiave esterna
-    - located.river: character varying(50), accetta valori NULL, chiave esterna
+    - city.name: character varying(50), non accetta valori NULL, parte della chiave primaria composta citykey
+    - city.latitude: numeric, accetta valori NULL
 3- Sottoproblemi:
+    - Selezionare tutte le città con latitudine > 66
+    - Selezionare tutte le città con latitudine < -66
 4- Assunzioni:
+    - si assume che i dati siano corretti
+    - si assume che i valori massimo e minimo di latitudine non superino 90 e -90
 5- Viste:
+    - nessuna vista richiesta
 6- Controllo del risultato:
-
+    - SELECT city.name, city.latitude FROM city WHERE latitude > 66 sia parte del risultato della query
+    - SELECT city.name, city.latitude FROM city WHERE latitude < -66 sia parte del risultato della query
 */
 
 SELECT city.name, city.latitude FROM city
@@ -84,12 +110,22 @@ Query 5:
 Elencare le nazioni che confinano con l'Italia (il risultato deve comprendere soltanto il nome delle
 nazioni).
 
-1- Informazioni correlate: Nome della nazione
-2- 
-3-
-4-
-5-
-6-
+1- Informazioni correlate: Nome della nazione, codice della nazione e il confine tra due nazioni
+2- Proprietà delle informazioni:
+    - country.name: character varying(50), non accetta valori NULL, valore univoco, non duplicabile
+    - country.code: character varying(4), non accetta valori NULL, chiave primaria countrykey
+    - borders.country1: character varying(4), non accetta valori NULL, parte della chiave primaria composta borderkey
+    - borders.country2: character varying(4), non accetta valori NULL, parte della chiave primaria composta borderkey
+3- Sottoproblemi:
+    - Selezionare tutti i confini che contengono il codice nazione I in qualsiasi dei due campi di borders
+    - Scartare tutti i confini il cui nome nazione corrisponde a "Italia"
+4- Assunzioni:
+    - si assume che i dati siano corretti
+    - si assume che i confini non vengano modificati spesso
+5- Viste:
+    - nessuna vista richiesta 
+6- Controllo del risultato:
+    - il risultato deve contenere "Francia", "Svizzera", "Austria", "Slovenia", "San Marino" e "Città del Vaticano"
 
 */
 
@@ -103,7 +139,10 @@ Trovare la capitale con più abitanti tra tutte le capitali (il risultato deve c
 capitale e la sua popolazione).
 
 1- Informazioni correlate: Nome della città, popolazione della città e capitale del Paese
-2- 
+2- Proprietà delle informazioni:
+    - city.name: character varying(50), non accetta valori NULL, parte della chiave primaria composta citykey
+    - city.population: numeric, accetta valori NULL 
+    - country.capital: character varying(50), accetta valori NULL
 3- Sottoproblemi: 
     - Selezionare la città con la maggior popolazione 
     - Selezionale la maggior popolazione
@@ -129,13 +168,16 @@ Trovare la capitale con più abitanti tra tutte le capitali (il risultato deve c
 capitale e la sua popolazione) 
 
 1- Informazioni correlate: Nome della città, popolazione della città e capitale del Paese
-2- 
+2- Proprietà delle informazioni:
+    - city.name: character varying(50), non accetta valori NULL, parte della chiave primaria composta citykey
+    - city.population: numeric, accetta valori NULL 
+    - country.capital: character varying(50), accetta valori NULL
 3- Sottoproblemi: 
     - Selezionare la città con la maggior popolazione 
     - Selezionale la maggior popolazione
 4- Assunzioni:
     - Si presuppone che tutte le città abbiano dati di popolazione non nulli
-6- Verifica del risultato:
+6- Verifica del risultato: 
         SELECT city.name, city.population
         FROM country JOIN city ON country.capital = city.name
         WHERE city.population IS NOT NULL
@@ -155,6 +197,13 @@ Per ogni città statunitense estrarre la sua popolazione e la superficie dei lag
 si affaccia (il risultato deve comprendere la città, la popolazione e l'area complessiva dei suoi laghi)
 (scrivere due versioni della query)
 
+1- Informazioni correlate: 
+2- Proprietà delle informazioni: 
+3- Sottoproblemi:
+4- Assunzioni:
+5- Viste:
+6- Controllo del risultato:
+
 */
 
 SELECT city.name, city.population, SUM(lake.area) 
@@ -170,6 +219,13 @@ Per ogni città statunitense estrarre la sua popolazione e la superficie dei lag
 si affaccia (il risultato deve comprendere la città, la popolazione e l'area complessiva dei suoi laghi)
 (scrivere due versioni della query)
 
+1- Informazioni correlate: 
+2- Proprietà delle informazioni: 
+3- Sottoproblemi:
+4- Assunzioni:
+5- Viste:
+6- Controllo del risultato:
+
 */
 
 -----
@@ -177,6 +233,14 @@ si affaccia (il risultato deve comprendere la città, la popolazione e l'area co
 /*
 
 Query 8
+
+
+1- Informazioni correlate: 
+2- Proprietà delle informazioni: 
+3- Sottoproblemi:
+4- Assunzioni:
+5- Viste:
+6- Controllo del risultato:
 
 */
 
